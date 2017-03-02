f33ca537644b767e423f04e9cdf1f40faa4cdac0
cmltdstudent <nb2748@tc.columbia.edu>
2017-03-01 20:37:37 -0500

    New project

---
+++ app/src/main/java/com/example/cmltdstudent/inclassassignment05_ningelb/Keys.java
@@ -0,0 +1,18 @@
+package com.example.cmltdstudent.inclassassignment05_ningelb;
+
+/**
+ * Created by cmltdstudent on 2/27/17.
+ */
+
+public interface Keys
+{
+
+   String NAME = "name";
+    String SIZE = "size";
+    String MESSAGE = "message";
+    String PLANET = "planet";
+
+
+
+
+}
